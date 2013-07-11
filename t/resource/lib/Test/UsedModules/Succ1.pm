8e09d1c152e3369f5908d7201c429723aa8a77b1
Taiki Kawakami <moznion@gmail.com>
2013-07-11 05:22:19 +0000

    Import of MOZNION/Test-UsedModules-0.02 from CPAN.
    gitpan-cpan-distribution: Test-UsedModules
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MOZNION/Test-UsedModules-0.02.tar.gz
    gitpan-cpan-author:       MOZNION
    gitpan-cpan-maturity:     released

--- t/resource/lib/Test/UsedModules/Succ1.pm
+++ t/resource/lib/Test/UsedModules/Succ1.pm
@@ -7,30 +7,4 @@ use utf8;
 use Cwd;
 my $cwd = getcwd();
 
-# Use by not omit
-use File::Basename ();
-File::Basename::dirname('foo');
-
-# Use by imported function by specifying
-use File::Spec::Functions qw/catdir/;
-catdir('foo');
-
-# Use as variable
-use FindBin;
-my $fb = $FindBin::Bin;
-
-# Use as quote
-use Encode;
-my $hash = +{
-    version => "$Encode::VERSION",
-};
-
-# Require
-require File::Spec;
-File::Spec->catfile('bar');
-
-# XXX Exceptional!
-# Exporter should be ignored.
-use Exporter;
-
 1;
